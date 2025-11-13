de762c7e0e491420c05da5b66eeba4f323d44b1a
DESKTOP-7CDUDD6\Elmer <kpluz.ee@gmail.com>
2025-11-14 06:22:42 +0800

    new files

---
+++ universe/js/audio-mappings.js
@@ -0,0 +1,25 @@
+// js/audio-mappings.js
+window.animationAudioMappings = {
+    // Elephant mappings
+    'elephant': {
+        'Idle Trumpet': 'universe/sounds/zoo/elephant.mp3'
+    },
+    
+    // Lion mappings
+    'lion': {
+        'Roarning': 'universe/sounds/zoo/lion.mp3',
+        'Fight Idle': 'universe/sounds/zoo/lion.mp3'
+    },
+    
+    // Hyena mappings
+    'hyena': {
+        'Howling': 'universe/sounds/zoo/hyena.mp3'
+    },
+    
+    // Cheetah mappings
+    'cheetah': {
+        'Fight Idle': 'universe/sounds/zoo/cheetah.mp3'
+    }
+    
+    // Add new animal mappings here as needed
+};
